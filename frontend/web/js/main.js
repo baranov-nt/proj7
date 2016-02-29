33e9ebbcd48b976eb8e6564daad9fde975ca2d63
phpnt <admin@phpnt.com>
2016-03-01 00:01:53 +0500

    commit 11

--- frontend/web/js/main.js
+++ frontend/web/js/main.js
@@ -4,12 +4,26 @@
 $(".img-container")
     .mouseenter(function(){
         //$(".img-container .img-container-content").children().css("display", "none");
-        $(this).children('.img-container-content').css("display","none");
-        $(this).children('.img-container-active-content').css("display","block");
+        $(this).children('.img-container-content').css({
+            "opacity":"1",
+            "display":"none",
+        }).show().animate({opacity:0}, 10);
+        $(this).children('.img-container-active-content').css({
+            "opacity":"0",
+            "display":"block",
+        }).show().animate({opacity:1}, 10);
     })
     .mouseleave(function(){
-        $(this).children('.img-container-active-content').css("display","none");
-        $(this).children('.img-container-content').css("display","block");
+        //$(this).children('.img-container-active-content').css("display","none");
+        //$(this).children('.img-container-content').css("display","block");
+        $(this).children('.img-container-active-content').css({
+            "opacity":"1",
+            "display":"none",
+        }).show().animate({opacity:0}, 10);
+        $(this).children('.img-container-content').css({
+            "opacity":"0",
+            "display":"block",
+        }).show().animate({opacity:1}, 10);
     });
 $('#modal-5').on('hidden.bs.modal', function (e) {
     $("#modal-5 iframe").attr("src", $("#modal-5 iframe").attr("src"));
